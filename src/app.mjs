6b0a8224888d4754a82fae87c305c4d9d1cc2155
gustavopc <gustavobarsan@gmail.com>
2022-06-04 18:19:35 -0300

    fix: remove no important lines

--- src/app.mjs
+++ src/app.mjs
@@ -3,10 +3,6 @@ import express from "express"
 const PORT = process.env.PORT || 8080
 
 const app = express()
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.disable("x-powered-by")
-
 app.get('/', (req, res) => res.send({message: "Desafio Ewally"}))
 // app.get('/boleto/:code')
 
