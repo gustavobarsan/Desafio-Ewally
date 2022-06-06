1eba74ef7db13c8df399b5ce5e47a7a8990d37d4
gustavopc <gustavobarsan@gmail.com>
2022-06-06 13:41:45 -0300

    feature: boleto

--- src/services/boleto.service.mjs
+++ src/services/boleto.service.mjs
@@ -1,19 +1,29 @@
 export const boletoService = async (code) => {
     
     if(code.length === 47) {
-        // const codigoDoBanco =
-        // const codigoMoeda =
-        // const digitoVerificador =
-        const valor = code.slice(-10)
-        console.log(valor)
+        const codigoDoBanco = code.slice(0,3)
+        console.log('codigo do banco', codigoDoBanco)
+
+        const codigoMoeda = code.slice(3,4)
+        console.log('codigo da Moeda', codigoMoeda)
+        
+        const digitoVerificador = code.slice(32,33)
+        console.log(digitoVerificador)
+        
         const fatorVencimento = code.slice(33, 37)
         console.log(fatorVencimento)
-        // campoLivre =
+        
+        const valor = code.slice(-10)
+        console.log(valor)
+        
+        const campoLivre = code.slice(4,9) + code.slice(10,20) + code.slice(21,31)
+        console.log('campo livre', campoLivre)
+        
         const dataFixa = new Date('07/10/1997')
         dataFixa.setDate(dataFixa.getDate()+Number(fatorVencimento))
         
         return {
-            barCode: '',
+            barCode: codigoDoBanco + codigoMoeda + digitoVerificador + fatorVencimento + valor + campoLivre,
             amount: Number(Number(valor)/100).toFixed(2),
             expirateDate: dataFixa.toLocaleDateString().split('/').reverse().join('-')
         }
